041de81c74b179f3063b89a1bd8e2b0c54225dd8
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2020-11-22 21:29:16 +0200

    Добавляет функцию создания разметки

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,24 @@
 import images from "../gallery-items.js";
+
+// функция создания разметки
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+    <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+    class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+      </a>
+      </li>
+      `;
+    })
+    .join("");
+}
